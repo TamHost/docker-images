91d38a50e743f2bb4a38568db42ecd99fd50a83b
Arci224 <maximusik11@seznam.cz>
2024-09-07 15:18:46 +0200

    add Images

--- games/mta/entrypoint.sh
+++ games/mta/entrypoint.sh
@@ -5,12 +5,9 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Print Node.js Version
-printf "\033[1m\033[33mcontainer@tamhost~ \033[0m%s\n" "node -v"
-node -v
-
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 printf "\033[1m\033[33mcontainer@tamhost~ \033[0m%s\n" "$MODIFIED_STARTUP"
-# shellcheck disable=SC2086
-eval "$MODIFIED_STARTUP"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
