03d30ad5e57c5b7f1c3fb9c5eced34f2fc323cd2
Arci224 <12967102+Arci224@users.noreply.github.com>
2025-08-13 23:24:08 +0200

    fix error

--- games/mta/entrypoint.sh
+++ games/mta/entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -f /usr/lib/x86_64-linux-gnu/libmysqlclient.so.16 ]; then
     echo "Stahuji libmysqlclient.so.16..."
     wget -q http://old-releases.ubuntu.com/ubuntu/pool/universe/m/mysql-dfsg-5.1/libmysqlclient16_5.1.31-1ubuntu2_amd64.deb -O /tmp/libmysqlclient16.deb
     dpkg -x /tmp/libmysqlclient16.deb /tmp/mysql
-    cp /tmp/mysql/usr/lib/x86_64-linux-gnu/libmysqlclient.so.16 /usr/lib/x86_64-linux-gnu/
+    cp /tmp/mysql/usr/lib/libmysqlclient.so.16 /usr/lib/x86_64-linux-gnu/
     rm -rf /tmp/mysql /tmp/libmysqlclient16.deb
 fi
 
