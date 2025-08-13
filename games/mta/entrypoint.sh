ce416f9aaee29c69f10c9467a3219e69da5162e1
Arci224 <12967102+Arci224@users.noreply.github.com>
2025-08-13 23:04:29 +0200

    add libmysqlclient

--- games/mta/entrypoint.sh
+++ games/mta/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd /home/container
 
+# Přidání staré MySQL knihovny, pokud chybí
+if [ ! -f /usr/lib/x86_64-linux-gnu/libmysqlclient.so.16 ]; then
+    echo "Stahuji libmysqlclient.so.16..."
+    wget -q http://old-releases.ubuntu.com/ubuntu/pool/universe/m/mysql-dfsg-5.1/libmysqlclient16_5.1.31-1ubuntu2_amd64.deb -O /tmp/libmysqlclient16.deb
+    dpkg -x /tmp/libmysqlclient16.deb /tmp/mysql
+    cp /tmp/mysql/usr/lib/x86_64-linux-gnu/libmysqlclient.so.16 /usr/lib/x86_64-linux-gnu/
+    rm -rf /tmp/mysql /tmp/libmysqlclient16.deb
+fi
+
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
